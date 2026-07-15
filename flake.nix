9a6942ada9d0195278806c4e2a5d2b02ba599187
Mathieu Moalic <matmoa@pm.me>
2026-07-15 16:00:18 +0200

    Release v0.8.0

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
 
     webBuild = pkgs.flutter.buildFlutterApplication {
       pname = "mont-web";
-      version = "0.1.0";
+      version = "0.8.0";
       src = pkgs.lib.cleanSource ./flutter;
       autoPubspecLock = ./flutter/pubspec.lock;
       targetFlutterPlatform = "web";
@@ -79,7 +79,7 @@
 
     package = pkgs.rustPlatform.buildRustPackage {
       pname = "mont";
-      version = "0.1.0";
+      version = "0.8.0";
       src = ./backend;
 
       cargoLock = {
@@ -112,17 +112,17 @@
 
     prebuilt = pkgs.stdenvNoCC.mkDerivation {
       pname = "mont";
-      version = "0.7.0";
+      version = "0.8.0";
 
       src = pkgs.fetchurl {
-        url = "https://github.com/MathieuMoalic/mont/releases/download/v0.7.0/mont-v0.7.0-x86_64-linux.tar.gz";
-        hash = "sha256-mxjwNgX7Mr710NMTo39goQd324jQPHHLZzbOfGID/bs=";
+        url = "https://github.com/MathieuMoalic/mont/releases/download/v0.8.0/mont-v0.8.0-x86_64-linux.tar.gz";
+        hash = "sha256-A5gUmY0zWHUMx1hPrQDQozLYLt8+kSc0gmItzGFFZm8=";
       };
 
       sourceRoot = ".";
 
       installPhase = ''
-        install -Dm755 mont-v0.7.0-x86_64-linux $out/bin/mont
+        install -Dm755 mont-v0.8.0-x86_64-linux $out/bin/mont
       '';
 
       meta = with lib; {
